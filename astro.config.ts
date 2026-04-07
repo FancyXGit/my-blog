85825c67acf6a7c17f665c957fdc63c73ee9de6b
FancyXGit <Fancy.fantasy688@gmail.com>
2026-04-07 22:17:22 +0800

    fix:修改地址

--- astro.config.ts
+++ astro.config.ts
@@ -115,7 +115,7 @@ export default defineConfig({
   // https://docs.astro.build/en/guides/prefetch/
   prefetch: true,
   // ! Please remember to replace the following site property with your own domain
-  site: "http://localhost:45873/",
+  site: "http://43.167.194.89:45873/",
   vite: {
     build: {
       sourcemap: true, // Source maps generation
