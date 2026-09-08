2b197aca94123bf1540fe15b915c683a0e8cc7b9
FancyXGit <Fancy.fantasy688@gmail.com>
2026-09-08 13:38:39 +0800

    link: 更新友链

--- src/data/links.ts
+++ src/data/links.ts
@@ -10,6 +10,30 @@ export const friendLinks: FriendLink[] = [
 		name: "FancyXGit",
 		description: "Drifting in the ocean all alone",
 		url: "https://fancyflow.top",
-		avatar: "https://github.com/FancyXGit.png",
+		avatar: "https://cdn.fancyflow.top/cat.png",
+	},
+	{
+		name: "Arthals' ink",
+		description: "所见高山远木，阔云流风；所幸岁月盈余，了无拘束",
+		url: "https://arthals.ink/",
+		avatar: "https://cdn.arthals.ink/Arthals.png",
+	},
+	{
+		name: "Wangxinyang",
+		description: "个人博客 / 学习交流 / 生活日常",
+		url: "https://wangxinyang.top",
+		avatar: "https://wangxinyang.top/avatar.png",
+	},
+	{
+		name: "Nian's Blog",
+		description: "一念起，万水千山",
+		url: "https://alannian.top",
+		avatar: "https://alannian.top/assets/avatar.jpg",
+	},
+	{
+		name: "Bertsin's Blog",
+		description: "神秘小天地",
+		url: "https://bertsin.pages.dev",
+		avatar: "https://i.imglt.com/20260214/c37821a7cc3ad4bf4717c03dfb6ecfce.jpg",
 	},
 ];
