15aabc3a2f437afc7e51f0c333790fdb956e8c89
FancyXGit <Fancy.fantasy688@gmail.com>
2026-09-08 12:41:51 +0800

    feat: 添加友链功能

---
+++ src/data/links.ts
@@ -0,0 +1,15 @@
+export interface FriendLink {
+	name: string;
+	description: string;
+	url: string;
+	avatar: string;
+}
+
+export const friendLinks: FriendLink[] = [
+	{
+		name: "FancyXGit",
+		description: "Drifting in the ocean all alone",
+		url: "https://fancyflow.top",
+		avatar: "https://github.com/FancyXGit.png",
+	},
+];
--- src/site.config.ts
+++ src/site.config.ts
@@ -41,7 +41,7 @@ export const menuLinks: { path: string; title: string }[] = [
 		title: "Notes",
 	},
 	{
-		path: "/music/",
-		title: "Music",
+		path: "/links/",
+		title: "Links",
 	},
 ];
